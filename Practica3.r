9f6e3ec7ce54bbd1502d2855c9706f37800e0b52
Ricardo A. Rodriguez <ra.rodriguezojeda@ugto.mx>
2023-07-11 07:25:00 -0600

    Primer Commit del documento practica 3 realizado en R Studio

--- Practica3.r
+++ Practica3.r
@@ -1,4 +1,12 @@
 # En este documento realizaremos un ejemplo de la practica de Laringoscopia
 #Comparacion de tratamientos para Laringoscopia.
 #### Humberto Martínez Bautista, María de Lourdes Delgadillo Hurtado, Ricardo Alberto Rodríguez Ojda
-R versión 4.3.1
+#R versión 4.3.1
+
+list.of.packages <- c("medicaldata", "janitor","dplyr","ggplot2","cowplot","gmodels","rstatix","psych","RColorBrewer","ggpubr","car","stats",
+                      "corrplot")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+data(laryngoscope, package="medicaldata" )
+datos <- laryngoscope
