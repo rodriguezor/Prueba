29e7142685aeb7915e7afde98cfd6d28b8efb2d9
Ricardo A. Rodriguez <ra.rodriguezojeda@ugto.mx>
2023-07-11 07:14:07 -0600

    Primer commit

---
+++ Practica3.r
@@ -0,0 +1,4 @@
+# En este documento realizaremos un ejemplo de la practica de Laringoscopia
+#Comparacion de tratamientos para Laringoscopia.
+#### Humberto Martínez Bautista, María de Lourdes Delgadillo Hurtado, Ricardo Alberto Rodríguez Ojda
+R versión 4.3.1
